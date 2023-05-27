c5c6b55558e2bdf2b5bc9742787bea4b45f37da8
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-27 18:19:44 +0300

    created server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { trainsRouter } from './routes/trainsRouter.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,8 +9,6 @@ const app = express();
 
 app.use(cors());
 
-app.use('/trains', (req, res) => {
-  res.end('HELLO');
-});
+app.use('/trains', trainsRouter);
 
 app.listen(PORT);
