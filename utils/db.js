8bef608d5ffac0c87072e161cea0ba9acedf2b42
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-27 23:07:15 +0300

    changed createConnection to createPool

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 import mysql12 from 'mysql2';
 
-export const connection = mysql12.createConnection({
+export const pool = mysql12.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
