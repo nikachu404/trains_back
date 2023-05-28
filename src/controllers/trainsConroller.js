30c279d55388de8ae7bcaa5aa45eca3d02f84df8
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-28 18:32:58 +0300

    added date qquery

--- src/controllers/trainsConroller.js
+++ src/controllers/trainsConroller.js
@@ -1,15 +1,23 @@
 import { pool } from '../../utils/db.js';
 
 export const getAvailableTrains = (req, res) => {
-  const { departure, arrival } = req.query;
+  const { departure, arrival, date } = req.query;
 
   let query = 'SELECT * FROM trains';
 
   const params = [];
 
-  if (departure && arrival) {
-    query += ' WHERE departure_station = ? AND arrival_station = ?';
-    params.push(departure, arrival);
+  if (departure) {
+    query += ' WHERE departure_station = ?';
+    params.push(departure, arrival, date);
+  }
+
+  if (arrival) {
+    query += ' AND arrival_station = ?';
+  }
+
+  if (date) {
+    query += ' AND DATE(departure_time) = ?';
   }
 
   pool.execute(query, params, (error, results) => {
